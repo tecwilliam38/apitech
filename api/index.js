037326243fa3e9d9461c6d95e737f9e7dbd7b447
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-07-20 09:10:50 -0300

    routes add

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-// import router from "./routes.js";
+import router from "./routes.js";
 
 const app = express();
 
