37a62ed0860719e22f252d3458e7f54f782fc1e9
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-07-20 09:05:25 -0300

    router

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import router from "./routes.js";
+// import router from "./routes.js";
 
 const app = express();
 
 
 app.use(express.json());
 app.use(cors());
-app.use(router);
+// app.use(router);
 app.use(bodyParser.json());
 
 app.listen(3001, ()=>{
@@ -16,4 +16,4 @@ app.listen(3001, ()=>{
 })
 
 
-export default app;
+// export default app;
