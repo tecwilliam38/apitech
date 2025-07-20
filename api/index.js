68f6f4440c24bfb818d70b259e820446f1b37544
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-07-20 08:58:32 -0300

    api

--- api/index.js
+++ api/index.js
@@ -11,9 +11,9 @@ app.use(cors());
 app.use(router);
 app.use(bodyParser.json());
 
-// app.listen(3001, ()=>{
-//     console.log("Aplicação rodando na porta:3001");    
-// })
+app.listen(3001, ()=>{
+    console.log("Aplicação rodando na porta:3001");    
+})
 
 
 export default app;
