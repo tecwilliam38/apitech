3c84f018808b31fba12722a6a18a0657e26ae84c
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-07-21 10:32:34 -0300

    rotas agendamentos

--- api/routes.js
+++ api/routes.js
@@ -7,8 +7,9 @@ const router = Router();
 import clientController from "./controllers/clientController.js";
 import admminController from "./controllers/admminController.js";
 import tecnicoController from "./controllers/tecnicoController.js";
+import appointmentController from "./controllers/appointmentController.js";
 // import controllerTecnico from "./controllers/controllerTecnico.js";
-// import controllerAppointment from "./controllers/controllerAppointment.js";
+// import controllerAppointment from "./controllers/appointmentController.js";
 // import controllerClient from "./controllers/controllerClient.js";
 // import pool from "./database/pool.index.js";
 
@@ -25,7 +26,7 @@ router.post('/client/buscar', jwt.ValidateToken, clientController.BuscarClient )
 router.post("/admin/register", admminController.InserirAdmin);
 router.post("/admin/login", admminController.LoginAdmin);
 // router.get("/admin/listar", jwt.ValidateToken, controllerAdmin.ListarAdmin);
-// router.get("/admin/appointments", jwt.ValidateToken, controllerAppointment.Listar);
+// router.get("/admin/appointments", jwt.ValidateToken, appointmentController.Listar);
 
 // Tecnicos...
 router.post("/tecnicos/register", jwt.ValidateToken, tecnicoController.InserirTecnico);
@@ -35,10 +36,11 @@ router.put("/tecnicos/:id_tecnico", jwt.ValidateToken, tecnicoController.EditarT
 router.get("/tecnicos/:id_tecnico/services", jwt.ValidateToken, tecnicoController.ListarServicosTecnico);
 
 // Appointments
-// router.post("/appointments/insert", jwt.ValidateToken, controllerAppointment.Inserir);
-// router.get("/appointments/listar", jwt.ValidateToken, controllerAppointment.ListarByUser);
-// router.get("/appointments/listar/:id_appointment", jwt.ValidateToken, controllerAppointment.ListarId);
-// router.put("/appointments/edit/:id_appointment", jwt.ValidateToken, controllerAppointment.EditarAdmin);
-// router.delete("/appointments/:id_appointment", jwt.ValidateToken, controllerAppointment.Excluir);
+router.post("/appointments/insert", jwt.ValidateToken, appointmentController.Inserir);
+router.get("/appointments/listar/tecnico/:id_tecnico", jwt.ValidateToken, appointmentController.ListarByUser);
+router.get("/appointments/listar", jwt.ValidateToken, appointmentController.ListarAll);
+router.get("/appointments/listar/:id_appointment", jwt.ValidateToken, appointmentController.ListarId);
+router.put("/appointments/edit/:id_appointment", jwt.ValidateToken, appointmentController.EditarAdmin);
+router.delete("/appointments/:id_appointment", jwt.ValidateToken, appointmentController.Excluir);
 
 export default router;
