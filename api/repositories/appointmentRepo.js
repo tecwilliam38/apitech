c58b7f317717bf24792a4517f4c1d82df64e87a6
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-08-07 09:24:05 -0300

    teste

--- api/repositories/appointmentRepo.js
+++ api/repositories/appointmentRepo.js
@@ -1,6 +1,43 @@
 import pool from "../database/db.js";
 
 
+
+async function ListarTecnico(id_client, dt_start, dt_end, id_tecnico, status) {
+
+    let filtro = [];
+
+    let sql = `select pa.id_appointment, ps.description as service, 
+    pt.name as tecnico, pt.specialty, pa.id_tecnico,
+   pa.booking_date, pa.booking_hour, pts.price as preco, pc.client_name as cliente,
+   pa.id_service, pa.status, pa.id_client
+   from apitech_appointments pa
+   join apitech_services ps on (ps.id_service = pa.id_service)
+   join apitech_tecnicos pt on (pt.id_tecnico = pa.id_tecnico)
+   join apitech_client pc on (pc.id_client = pa.id_client)
+   join apitech_tecnicos_services pts on (pts.id_tecnico = pa.id_tecnico and 
+                          pts.id_service = pa.id_service)
+   where pa.id_appointment > 0`;
+
+   if (dt_start) {
+        filtro.push(dt_start);
+        sql += " AND pa.booking_date >= $" + filtro.length;
+    }
+
+    if (dt_end) {
+        filtro.push(dt_end);
+        sql += " AND pa.booking_date <= $" + filtro.length;
+    }
+
+    if (id_tecnico) {
+        filtro.push(id_tecnico);
+        sql += " AND pa.id_tecnico = $" + filtro.length;
+    }
+    sql += " order by pa.id_tecnico";
+
+    const appointments = await pool.query(sql, filtro);
+
+    return appointments.rows;
+}
 async function ListarAll(id_client, dt_start, dt_end, id_tecnico, status) {
 
     let filtro = [];
