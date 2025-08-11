8d13e8f13e4a564844ce2699cdf57a297141d9b8
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-08-11 16:39:10 -0300

    hoje

--- api/database/neon.js
+++ api/database/neon.js
@@ -1,13 +1,6 @@
 import { Pool } from "pg";
 const URL = 'postgresql://neondb_owner:npg_nMLpeEIz0l5K@ep-summer-morning-acw5r4fj-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require';
 
-
-// const pool = new Pool({
-//   connectionString: URL,
-//   ssl: {
-//     rejectUnauthorized: false, // Neon usa SSL obrigatório
-//   },
-// });
 const pool = new Pool({
   host: 'ep-summer-morning-acw5r4fj-pooler.sa-east-1.aws.neon.tech',
   database: 'neondb',
