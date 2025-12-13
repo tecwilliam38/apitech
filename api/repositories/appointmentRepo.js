2f195df5c6b2bb89fbd6c356b7e237c5dde74e32
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-12-13 08:58:18 -0300

    up

--- api/repositories/appointmentRepo.js
+++ api/repositories/appointmentRepo.js
@@ -17,13 +17,29 @@ async function ListarAgenda(id_client, dt_start, dt_end, id_user) {
             c.client_name AS cliente,
             pa.id_service,
             pa.status,
-            pa.id_client
+            pa.id_client,
+            usv.price as preco
         FROM appointments pa
         JOIN user_skill us ON us.id_service = pa.id_service
         JOIN users u ON u.id_user = pa.id_user
         JOIN client c ON c.id_client = pa.id_client
+        JOIN user_services usv ON usv.id_user = pa.id_user 
+                        AND usv.id_service = pa.id_service
         WHERE pa.id_appointment > 0
     `;
+
+    // SELECT 
+    //     pa.id_appointment, pa.id_user, us.description AS service,
+    //     u.user_name AS tecnico, u.user_skill AS specialty, pa.booking_date,
+    //     pa.booking_hour, usv.price AS preco, c.client_name AS cliente,
+    //     pa.id_service, pa.status, pa.id_client
+    // FROM appointments pa
+    // JOIN user_skill us ON us.id_service = pa.id_service
+    // JOIN users u ON u.id_user = pa.id_user
+    // JOIN client c ON c.id_client = pa.id_client
+    // JOIN user_services usv ON usv.id_user = pa.id_user 
+    //                     AND usv.id_service = pa.id_service
+    // WHERE pa.id_appointment > 0;
 
     if (id_client) {
         filtro.push(id_client);
