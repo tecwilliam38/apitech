35b5199153db65da19a8f8ff8828918c8df0fab2
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-12-13 09:15:16 -0300

    fff

--- api/repositories/appointmentRepo.js
+++ api/repositories/appointmentRepo.js
@@ -8,9 +8,9 @@ async function ListarAgenda(id_client, dt_start, dt_end, id_user) {
         SELECT 
             pa.id_appointment,
             pa.id_user,
-            us.description AS service,
-            u.user_name AS tecnico,
-            u.user_skill AS specialty,
+            us.description as service,
+            u.user_name as tecnico,
+            u.user_skill as specialty,
             u.user_genre as genre,
             pa.booking_date as date,
             pa.booking_hour as hour, 
@@ -26,20 +26,7 @@ async function ListarAgenda(id_client, dt_start, dt_end, id_user) {
         JOIN user_services usv ON usv.id_user = pa.id_user 
                         AND usv.id_service = pa.id_service
         WHERE pa.id_appointment > 0
-    `;
-
-    // SELECT 
-    //     pa.id_appointment, pa.id_user, us.description AS service,
-    //     u.user_name AS tecnico, u.user_skill AS specialty, pa.booking_date,
-    //     pa.booking_hour, usv.price AS preco, c.client_name AS cliente,
-    //     pa.id_service, pa.status, pa.id_client
-    // FROM appointments pa
-    // JOIN user_skill us ON us.id_service = pa.id_service
-    // JOIN users u ON u.id_user = pa.id_user
-    // JOIN client c ON c.id_client = pa.id_client
-    // JOIN user_services usv ON usv.id_user = pa.id_user 
-    //                     AND usv.id_service = pa.id_service
-    // WHERE pa.id_appointment > 0;
+    `;    
 
     if (id_client) {
         filtro.push(id_client);
