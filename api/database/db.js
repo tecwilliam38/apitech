e1c83bc408b06937822c475747c650cc10c43c2c
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2026-01-17 19:51:19 -0300

    ddd

--- api/database/db.js
+++ api/database/db.js
@@ -1,21 +1,6 @@
 import pg from 'pg'
 const { Pool } = pg
 
-// const pool = new Pool({
-//      host: 'aws-1-sa-east-1.pooler.supabase.com',     
-//      user: 'postgres.sgqzrloneflvfhumzwpt',     
-//      database: 'postgres',
-//      password: 'Estacio091@@',
-//      port: 6543,
-// });
-// const pool = new Pool({
-//      user: 'postgres.yulykztzhmoxfztykeop',     
-//      host: 'aws-0-sa-east-1.pooler.supabase.com',     
-//      database: 'postgres',
-//      password: 'Estacio091@@',
-//      port: 6543,
-// });
-
 const pool = new Pool({
      host: 'aws-1-sa-east-1.pooler.supabase.com',     
      user: 'postgres.cfnqsikankipkyfzypmr',     
